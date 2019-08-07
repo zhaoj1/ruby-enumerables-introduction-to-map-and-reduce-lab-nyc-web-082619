bcab74ab66457072b8d5ab19f7e9a981234f69c0
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-08-07 22:54:22 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -47,8 +47,7 @@ end
 
 def reduce_to_any_true(array)
   array.length.times{ |index|
-    if array[index] = nil
-    elsif array[index] = false
+    if array[index] = nil || false
     else
       return array[index]
     end
